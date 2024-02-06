8b4ac29e31775ffb694068d5e19657a05c81920d
Maksim P <pojilcov.my@rcitsakha.ru>
2024-02-06 17:52:05 +0900

    asyncAwait 1.2

--- fetch.js
+++ fetch.js
@@ -1,13 +1,25 @@
-fetch("https://fakestoreapi.com/productss",{
-    method: "GET",
-    headers: { 'Content-Type': 'application/json' },
-})
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
+// fetch("https://fakestoreapi.com/productss",{
+//     method: "GET",
+//     headers: { 'Content-Type': 'application/json' },
+// })
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((error) => {
+//     console.log(`Возникла ошибка: ${error}`);
+//   });
+
+async function fetchProducts() {
+  try {
+    const res = await fetch("https://fakestoreapi.com/produts");
+    const data = await res.json();
     console.log(data);
-  })
-  .catch((error) => {
-    console.log(`Возникла ошибка: ${error}`);
-  });
+  } catch (error) {
+    console.log(`КОРОЧЕ ВОТ ТАКАЯ ОШИБОЧКА: ${error}`);
+  }
+}
+
+fetchProducts();
